87144f151b0d347ac8faf4873ecfbbd27b4479f3
Viktor <lulzsec123@yahoo.com>
2024-03-26 18:57:36 +0200

    implemented most of requirements

--- client/scripts.js
+++ client/scripts.js
@@ -2,6 +2,19 @@ const api = "http://localhost:8080/cards"
 
 const display = document.querySelector("#display-data")
 
+const cardButton = (text, category, link) => {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.classList.add(`category_${category}`);
+    button.addEventListener('click', () => {
+        console.log(link);
+        window.location.href = link;
+    });
+
+    
+    return button.outerHTML;
+};
+
 
 const getData = async () => {
     const res = await fetch(api);
@@ -13,31 +26,30 @@ const getData = async () => {
 const displayData = async () => {
     const payload = await getData();
 
-    let dataDisplay = payload?.parks.map((e) => {
-
+    let dataDisplay = payload?.parks.map((e, index) => {
+        if (!e.image) return;
 
         return `
-        <section class="custom_card">
-          <header class="banner category_${e.category}">${e.category}</header>
+        <section id="${index === 1 ? `featured` : ``}" class="custom_card" >
+          
           <article>
-            <article>
-              <header>
+          <header class="banner category_${e.category}">${e.category}</header>
+              <figure>
                 <img src=${e.image} alt="image" />
-              </header>
+              </figure>
               <section class="card-profile__info">
-                <h2 class="card_title">${e.title}</h2>
+                <h2 class="card_title">${e.title ? e.title : 'National Park'}</h2>
                 <p class="card_body">${e.body}</p>
-                <section class="card_link_section">
-                    <button class="category_${e.category}">Find Out More</button>
-                </section>
               </section>
-            </article>
           </article>
+          ${e.link ? `
+                <section class="card_link_section">
+                  ${cardButton(e.linkText, e.category, e.link)}
+                </section>
+              ` : ''}
         </section>
         `
     }).join("")
-
-    console.log(dataDisplay)
 
 
     display.innerHTML = dataDisplay;
