5b409d63361f71404e8aa3c0c9963a22f31c9879
Viktor <lulzsec123@yahoo.com>
2024-03-27 14:06:18 +0200

    vercel: mod

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+const path = require('path');
 
 app.listen(
     PORT,
@@ -10,7 +12,9 @@ app.listen(
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(express.static('client'));
 
 app.get('/cards', (req, res) => {
     res.status(200).send({
