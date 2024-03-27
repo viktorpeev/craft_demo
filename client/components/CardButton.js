054608d869733c157ffc3297b0581e01ecd5f2b9
Viktor <lulzsec123@yahoo.com>
2024-03-27 13:23:13 +0200

    vercel: mod

--- client/components/CardButton.js
+++ client/components/CardButton.js
@@ -1,7 +1,7 @@
-const cardButton = (text, category, link) => {
-    return `
-      <button onclick="window.location.href = '${link}'" class="custom_card__button custom_card__category_${category}">${text}</button>
-    `;
-};
+// const cardButton = (text, category, link) => {
+//     return `
+//       <button onclick="window.location.href = '${link}'" class="custom_card__button custom_card__category_${category}">${text}</button>
+//     `;
+// };
 
-export default cardButton;
+// export default cardButton;
